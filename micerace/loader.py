d0be091db78eddd06d9fbc0f6392c0060b148d84
trevorprater <trevor.prater@gmail.com>
2019-09-14 20:11:01 -0400

    Adds 90% val_acc model

--- micerace/loader.py
+++ micerace/loader.py
@@ -3,60 +3,43 @@ import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv1D, MaxPooling1D
-from tensorflow.keras import optimizers
-#from tf.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+from datetime import datetime
 
-EPOCHS = 2000
 NUM_CLASSES = 4
-BATCH_SIZE = 32
-
 
 df = pd.read_csv('micerace/training_data_full.csv')
 winners = df['winner_position_ndx']
-clean_df = df.drop(
-    columns=[
-        'winner_position_ndx', 'mice', 'winner_name', 'race_id', 'mouse_0_name', 'mouse_1_name', 'mouse_2_name', 'mouse_3_name'])
 
+clean_df = df.drop(
+    columns=['winner_position_ndx',
+             'mice',
+             'winner_name',
+             'race_id',
+             'mouse_0_name',
+             'mouse_1_name',
+             'mouse_2_name',
+             'mouse_3_name'])
+
+clean_df = clean_df.fillna(value=0)
 
 y_data = winners.to_numpy(dtype=int)
 x_data = clean_df.to_numpy(dtype=float)
 x_data = np.expand_dims(x_data, axis=2)
 
 
-(x_train, y_train) = (x_data[:int(len(x_data)*0.7)], y_data[:int(len(y_data)*0.7)])
-(x_test, y_test) = (x_data[int(len(x_data)*0.7):], y_data[int(len(y_data)*0.7):])
-
-#(x_data[int(len(x_data)*0.8):]], y_data[:70]), (x_data[70:], y_data[70:])
-
+(x_train, y_train) = (x_data[:int(len(x_data)*0.7)], to_categorical(y_data[:int(len(y_data)*0.7)], num_classes=4))
+(x_test, y_test) = (x_data[int(len(x_data)*0.7):], to_categorical(y_data[int(len(y_data)*0.7):], num_classes=4))
 
 model = Sequential()
-print(x_train.shape)
-print(x_train.shape[1:])
-
-model.add(Conv1D(100, 100, padding='same', input_shape=x_train.shape[1:])) #x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv1D(100, 100))
-model.add(Activation('relu'))
-model.add(MaxPooling1D(pool_size=1))
-model.add(Dropout(0.25))
-
-model.add(Conv1D(100, 100, padding='same'))
-model.add(Activation('relu'))
-model.add(Conv1D(100, 1))
-model.add(Activation('relu'))
-model.add(MaxPooling1D(pool_size=1))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=x_train.shape[1:]))
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
 model.add(Dropout(0.5))
+model.add(MaxPooling1D(pool_size=2))
 model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.add(Dense(NUM_CLASSES))
-model.add(Activation('softmax'))
-
-opt = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-
-model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_test, y_test), shuffle=False)
+model.fit(x_train, y_train, batch_size=32, epochs=50, validation_data=(x_test, y_test), shuffle=True)
+model.save(f'model-{datetime.utcnow()}.h5')
